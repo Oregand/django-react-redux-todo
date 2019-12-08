c1eb97d3a95a93c8738e43f024e7d31c0aeb0bd2
David <davidoregan91@gmail.com>
2019-12-08 20:22:03 +0000

    feat(client): frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [viewCompleted, setViewCompleted] = useState(false)
+  const [todoList, setTodoList] = useState([
+    {
+      id: 1,
+      title: "Go to Market",
+      description: "Buy ingredients to prepare dinner",
+      completed: true
+    },
+    {
+      id: 2,
+      title: "Study",
+      description: "Read Algebra and History textbook for upcoming test",
+      completed: false
+    },
+    {
+      id: 3,
+      title: "Sally's books",
+      description: "Go to library to rent sally's books",
+      completed: true
+    },
+    {
+      id: 4,
+      title: "Article",
+      description: "Write article on how to use django with react",
+      completed: false
+    }
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="content">
+      <h1 className="text-white text-uppercase text-center my-4">Todo app</h1>
+      <div className="row ">
+        <div className="col-md-6 col-sm-10 mx-auto p-0">
+          <div className="card p-3">
+            <div className="">
+              <button className="btn btn-primary">Add task</button>
+            </div>
+            <div className="my-5 tab-list">
+              <span
+                onClick={() => setViewCompleted(true)}
+                className={viewCompleted ? "active" : ""}
+              >
+                complete
+              </span>
+              <span
+                onClick={() => setViewCompleted(false)}
+                className={viewCompleted ? "" : "active"}
+              >
+                Incomplete
+              </span>
+            </div>
+            <ul className="list-group list-group-flush">
+              {todoList.map(item => (
+                <li
+                  key={item.id}
+                  className="list-group-item d-flex justify-content-between align-items-center"
+                >
+                  <span
+                    className={`todo-title mr-2 ${
+                      viewCompleted ? "completed-todo" : ""
+                    }`}
+                    title={item.description}
+                  >
+                    {item.title}
+                  </span>
+                  <span>
+                    <button className="btn btn-secondary mr-2"> Edit </button>
+                    <button className="btn btn-danger">Delete </button>
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
