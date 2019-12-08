8b366a3389c25542afbcc3f8c4146db8777aad2a
David <davidoregan91@gmail.com>
2019-12-08 00:24:58 +0000

    feat(server): add todo view

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets          # add this
+from .serializers import TodoSerializer      # add this
+from .models import Todo   
 
 # Create your views here.
+
+class TodoView(viewsets.ModelViewSets):
+    serializer_class = TodoSerializer
+    queryset = Todo.objects.all()
